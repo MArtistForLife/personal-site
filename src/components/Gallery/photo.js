362caba3f63da1e2149dda77263b441eb2ee2471
Lijun(Stephen) Zhu <89487682+Lijun56@users.noreply.github.com>
2024-07-01 14:09:55 -0400

    update one again

--- src/components/Gallery/photo.js
+++ src/components/Gallery/photo.js
@@ -7,7 +7,9 @@ const PhotoCard = ({ data }) => (
     <div className="card-body">
       <h5 className="card-title">{data.caption}</h5>
       <p className="card-text">Price: ${data.price}</p>
-      <img src={`${process.env.PUBLIC_URL}/images/functionIcon/ebayCart.png`} alt="eBay Cart" className="card-icon" />
+      <a href={data.ebayLink} className="card-link">
+        <img src={`${process.env.PUBLIC_URL}/images/functionIcon/ebayCart.png`} alt="eBay Cart" className="card-icon" />
+      </a>
     </div>
   </div>
 );
